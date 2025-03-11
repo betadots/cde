a338209bdfce374416686689cebdcaf21ee266e1
Lennart Betz <lbetz@prefork.de>
2025-03-11 10:18:03 +0100

    Replace an idempotent output for templates

--- modules/proxmox/outputs.tf
+++ modules/proxmox/outputs.tf
@@ -1,5 +1,11 @@
 output "templates" {
-  value = data.proxmox_virtual_environment_vms.templates
+  value = {
+    for instance in data.proxmox_virtual_environment_vms.templates.vms:
+    instance.name => {
+      id = instance.vm_id
+      node = instance.node_name
+    }
+  }
 }
 
 output "ipv4" {
